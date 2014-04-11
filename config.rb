85ca0dc74d27a04c1d353210f787531de6082b93
carolanncan <carolanncan@gmail.com>
2014-04-10 21:34:58 -0700

    Add middleman-deploy gem, add build configurations

--- config.rb
+++ config.rb
@@ -1,75 +1,18 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-helpers do
-  def magic_link_to(link, url, opts={})
-      current_url = current_resource.url
-      if current_url == url_for(url) || current_url == url_for(url) + "/"
-          opts[:class] = "active"
-      end
-      link_to(link, url, opts)
-  end
+activate :deploy do |deploy|
+  deploy.branch   = "gh-pages"
+  #deploy.build_before = true
+  deploy.method = :git
 end
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
 activate :livereload
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :build_dir, "tmp"
-
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
-
 set :relative_links, true
 
-# Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
 end
