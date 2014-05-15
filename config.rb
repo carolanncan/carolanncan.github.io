517ad2ecc9a549d5ea2882cfbc1c3deef8cfcd91
carolanncan <carolanncan@gmail.com>
2014-05-14 22:56:59 -0700

    Relative links config

--- config.rb
+++ config.rb
@@ -15,9 +15,10 @@ activate :livereload
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
-#set :relative_links, true
+set :relative_links, true
 
 configure :build do
   activate :minify_css
   activate :minify_javascript
+  activate :relative_links
 end
